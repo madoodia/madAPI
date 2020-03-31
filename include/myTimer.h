31af26a4a16f863a11ad35e3e4a323de0e882a2e
Mahmoodreza Aarabi <madoodia@gmail.com>
2020-03-30 19:13:20 -0700

    using .clang-format

--- include/myTimer.h
+++ include/myTimer.h
@@ -1,34 +1,34 @@
 #ifndef MYTIMER_H
 #define MYTIMER_H
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 struct Timer
 {
-	std::chrono::steady_clock::time_point start, end;
-	std::chrono::duration<float> duration = {};
-	double endResult = 0.0;
+  std::chrono::steady_clock::time_point start, end;
+  std::chrono::duration<float> duration = {};
+  double endResult = 0.0;
 
-	Timer()
-	{
-		start = std::chrono::steady_clock::now();
-	}
+  Timer()
+  {
+    start = std::chrono::steady_clock::now();
+  }
 
-	~Timer()
-	{
-		endResult = result();
-		std::cout << endResult << "ms\n";
-	}
+  ~Timer()
+  {
+    endResult = result();
+    std::cout << endResult << "ms\n";
+  }
 
-	double result()
-	{
-		end = std::chrono::steady_clock::now();
-		duration = end - start;
+  double result()
+  {
+    end = std::chrono::steady_clock::now();
+    duration = end - start;
 
-		endResult = (double)duration.count() * (double)1000.0f;
-		return endResult;
-	}
+    endResult = (double)duration.count() * (double)1000.0f;
+    return endResult;
+  }
 };
 
 #endif // MYTIMER_H
